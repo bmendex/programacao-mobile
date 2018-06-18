9baa91bbddc180ca7a1dc55507d0dca3a306751a
bmendex <bronu.mendes@gmail.com>
2018-06-17 23:42:03 -0300

    TheBestBeer

--- projeto/TheBestBeer/app/src/main/java/thebestbeer/br/com/thebestbeer/activities/LoginActivity.java
+++ projeto/TheBestBeer/app/src/main/java/thebestbeer/br/com/thebestbeer/activities/LoginActivity.java
@@ -2,14 +2,15 @@ package thebestbeer.br.com.thebestbeer.activities;
 
 import android.app.Activity;
 import android.databinding.DataBindingUtil;
+import android.os.Handler;
 import android.renderscript.ScriptGroup;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
+import android.view.View;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -19,6 +20,7 @@ import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 
 import thebestbeer.br.com.thebestbeer.R;
@@ -28,61 +30,79 @@ import thebestbeer.br.com.thebestbeer.util.Log;
 @EActivity(R.layout.activity_login)
 public class LoginActivity extends AppCompatActivity {
 
-    final CallbackManager callbackManager = CallbackManager.Factory.create();
-
-  //  ---------------------------------------------------------------------
-  //   ATRIBUTOS
-  //  ---------------------------------------------------------------------
+    //---------------------------------------------------------------------
+    // ATRIBUTOS
+    //---------------------------------------------------------------------
 
     private ActivityLoginBinding mBinding;
 
-  //  ---------------------------------------------------------------------
-  //   METODOS
-  //  ---------------------------------------------------------------------
+    //---------------------------------------------------------------------
+    // METODOS
+    //---------------------------------------------------------------------
     @Override
-    protected void onCreate(@Nullable final Bundle savedInstanceState) {
+    protected void onCreate(@Nullable final Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        Log.d("LoginActivity.onCreate");
+
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
-
     }
 
+    /**
+     * Este método é chamado assim que as views forem inicializados no
+     * onCreate()
+     */
     @AfterViews
-    protected void initi() {
+    protected void initi(){
 
-        Log.d("LoginActivity.initi");
-
-        // Inicializa o databinding
+        // Inicializa o data binding
         mBinding = ActivityLoginBinding.inflate(getLayoutInflater());
 
-       // mBinding.loginButton.setReadPermissions("email");
+        // Acessando views no xml
+        mBinding.loginButton.setVisibility(View.VISIBLE);
 
-
-        // Callback registration
+        final CallbackManager callbackManager = CallbackManager.Factory.create();
+    //      Callback registration
         mBinding.loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                // App code
-                Log.d("LoginActivity.onSuccess");
-                Toast.makeText(LoginActivity.this, "onSuccess", Toast.LENGTH_SHORT).show();
-                //HomeActivity_.intent(LoginActivity.this).start();
-            }
+                    @Override
+                    public void onSuccess(final LoginResult loginResult) {
+                        // Se sucesso acessar home
+                        Toast.makeText(LoginActivity.this, "Deu boa", Toast.LENGTH_SHORT).show();
 
-            @Override
-            public void onCancel() {
-                // App code
-                Log.d("LoginActivity.onCancel");
-                Toast.makeText(LoginActivity.this, "onCancel", Toast.LENGTH_SHORT).show();
-            }
+                        Log.d("LoginActivity.onSuccess: "+ loginResult.getAccessToken());
 
-            @Override
-            public void onError(FacebookException exception) {
-                // App code
-                Log.d("LoginActivity.onError");
-                Toast.makeText(LoginActivity.this, "onErro", Toast.LENGTH_SHORT).show();
-            }
-        });
+                        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+                        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+                    }
 
+                    @Override
+                    public void onCancel() {
+                        // Se cancelar voltar para tela de login
+                        Toast.makeText(LoginActivity.this, "Não deu boa", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // Se erro setar mensagem
+                        Toast.makeText(LoginActivity.this, "ERRO", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
+        // Exibe a tela de login por 3 segundos e apresenta a tela inicial
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(LoginActivity.this, "FOI", Toast.LENGTH_SHORT).show();
+            }
+        },1000);
+
+//        mBinding.entrarSemlgnFb.setVisibility(View.VISIBLE);
+//        mBinding.entrarSemlgnFb.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                HomeActivity_.intent(LoginActivity.this).start();
+//            }
+//        });
     }
 }
